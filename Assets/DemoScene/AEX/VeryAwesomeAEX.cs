65fa98c59e0dc928015bda87f20920f26a752c8b
Heejin <monster@teamappetizer.com>
2016-08-07 03:05:26 +0900

    Add coroutine usage in AEX

--- Assets/DemoScene/AEX/VeryAwesomeAEX.cs
+++ Assets/DemoScene/AEX/VeryAwesomeAEX.cs
@@ -1,9 +1,9 @@
-﻿using UnityEngine;
+﻿using ActionEngine;
 using System.Collections;
-using ActionEngine;
+using UnityEngine;
 
 public static class VeryAwesomeAEX {
-	
+
 	public static ActionBase Create (IAEScriptContext ctx) {
 		var simpleAEX = ctx.GetAEScript("$simple");
 		var cube = ctx.GetTransform("$cube");
@@ -14,17 +14,24 @@ public static class VeryAwesomeAEX {
 				AE.Parallel(
 					// You can use another AEX
 					simpleAEX.Create(),
+
 					// Basic tweens
 					cube.AEMove(new Vector3(0, -3, 0), 3.5f).Easing(Easings.BounceInOut),
 					sphere.AEMove(new Vector3(0, 3, 0), 4.5f).Easing(Easings.ElasticOut)
-                ),
+				),
 				AE.Parallel(
 					cube.AEMove(new Vector3(0, 3, 0), 2.5f).Relative(true).Easing(Easings.BackOut),
 					sphere.AEMove(new Vector3(0, -3, 0), 3.5f).Relative(true).Easing(Easings.QuadOut)
 				),
-				AE.Delay(0.5f),
+				AE.Coroutine(() => ExampleCoroutine()),
 				AE.Debug("All Completed!")
 			);
+	}
+
+	private static IEnumerator ExampleCoroutine () {
+		Debug.Log("Coroutine Started: " + Time.time);
+		yield return new WaitForSeconds(2f);
+		Debug.Log("Coroutine End: " + Time.time);
 	}
 
 }
